1f55b08d6da2b40428b6cfabd0d107228fb766de
dennisvg111 <dennisvg111@gmail.com>
2023-05-23 13:41:05 +0200

    Add standard tags

--- DG.Yaml/src/Model/Tag.cs
+++ DG.Yaml/src/Model/Tag.cs
@@ -2,6 +2,19 @@
 {
     public class Tag
     {
+        public static Tag Sequence { get; } = new Tag("tag:yaml.org,2002:seq");
+        public static Tag Map { get; } = new Tag("tag:yaml.org,2002:map");
+        public static Tag Null { get; } = new Tag("tag:yaml.org,2002:null");
+        public static Tag Bool { get; } = new Tag("tag:yaml.org,2002:bool");
+        public static Tag Int { get; } = new Tag("tag:yaml.org,2002:int");
+        public static Tag Float { get; } = new Tag("tag:yaml.org,2002:float");
+        public static Tag String { get; } = new Tag("tag:yaml.org,2002:str");
+
         public string Name { get; set; }
+
+        public Tag(string name)
+        {
+            Name = name;
+        }
     }
 }
