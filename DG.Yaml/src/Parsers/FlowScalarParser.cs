d481a9af31a9258ed5fb80c1b97f7155649fbdfe
dennisvg111 <dennisvg111@gmail.com>
2023-08-03 17:53:48 +0200

    Add initial flow scalar parser stub

---
+++ DG.Yaml/src/Parsers/FlowScalarParser.cs
@@ -0,0 +1,20 @@
+﻿namespace DG.Yaml.Parsers
+{
+    public class FlowScalarParser
+    {
+        private readonly bool _isSingleQuote;
+        private readonly LineReader _reader;
+
+        private readonly Scalar _scalar = new Scalar();
+
+        public FlowScalarParser(LineReader reader, bool isSingleQuote)
+        {
+            _reader = reader;
+            _isSingleQuote = isSingleQuote;
+
+            _scalar = new Scalar();
+        }
+
+
+    }
+}
---
+++ DG.Yaml/src/Parsers/Scalar.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace DG.Yaml.Parsers
+{
+    public class Scalar
+    {
+        private readonly static StringBuilder _builder = new StringBuilder();
+
+        private readonly List<char> _buffer;
+
+        public int Length => _buffer.Count;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Write(char c)
+        {
+            _buffer.Add(c);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Clear()
+        {
+            _buffer.Clear();
+        }
+
+        public override string ToString()
+        {
+            lock (_builder)
+            {
+                _builder.Clear();
+                foreach (char c in _buffer)
+                {
+                    _builder.Append(c);
+                }
+                return _builder.ToString();
+            }
+        }
+    }
+}
